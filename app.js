42b76dca8f59f5121543399a42f9de33a7685d10
masukyus <109662507+masukyus@users.noreply.github.com>
2023-05-28 12:28:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -47,3 +47,105 @@ const T = new Twit({
 });
 
 // Twitter API isteklerini burada gerçekleştirin
+require('dotenv').config(); // .env dosyasını yüklemek için
+
+const Twit = require('twit');
+
+const T = new Twit({
+
+  consumer_key: process.env.TWITTER_API_KEY,
+
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+});
+
+// Kullanıcının takipçilerini almak için API isteği gönderme
+
+T.get('followers/list', { screen_name: 'kullanici_adi' }, (err, data, response) => {
+
+  if (err) {
+
+    console.error('Takipçiler alınamadı:', err);
+
+    return;
+
+  }
+
+  
+
+  const followers = data.users;
+
+  console.log('Takipçiler:', followers);
+
+});
+
+// Bir tweetin beğeni sayısını almak için API isteği gönderme
+
+T.get('statuses/show/:id', { id: 'tweet_id' }, (err, data, response) => {
+
+  if (err) {
+
+    console.error('Tweet alınamadı:', err);
+
+    return;
+
+  }
+
+  
+
+  const likesCount = data.favorite_count;
+
+  console.log('Beğeni Sayısı:', likesCount);
+
+});
+
+// Bir tweete yanıt olarak tweet atma
+
+const tweet = {
+
+  status: 'Merhaba, bu bir yanıttır!',
+
+  in_reply_to_status_id: 'yanit_olarak_atilan_tweet_id'
+
+};
+
+T.post('statuses/update', tweet, (err, data, response) => {
+
+  if (err) {
+
+    console.error('Tweet atılamadı:', err);
+
+    return;
+
+  }
+
+  
+
+  console.log('Tweet atıldı:', data);
+
+});
+
+// Bir tweeti retweet etme
+
+T.post('statuses/retweet/:id', { id: 'retweet_edilecek_tweet_id' }, (err, data, response) => {
+
+  if (err) {
+
+    console.error('Retweet edilemedi:', err);
+
+    return;
+
+  }
+
+  
+
+  console.log('Retweet edildi:', data);
+
+});
+
+
+
